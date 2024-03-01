168a00c5e9fd99471963e42eea85df43f6a44451
wangchao777 <625686833@qq.com>
2024-03-01 12:52:03 +0800

    Update controller.go

--- pkg/operator/ceph/controller.go
+++ pkg/operator/ceph/controller.go
@@ -141,7 +141,6 @@ func reconcileOperatorLogLevel(data map[string]string) {
 }
 
 func (r *ReconcileConfig) reconcileDiscoveryDaemon(data map[string]string) error {
-	return nil
 	rookDiscover := discover.New(r.context.Clientset)
 	if opcontroller.DiscoveryDaemonEnabled(r.config.Parameters) {
 		if err := rookDiscover.Start(r.opManagerContext, r.config.OperatorNamespace, r.config.Image, r.config.ServiceAccount, data, true); err != nil {
